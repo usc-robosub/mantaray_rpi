e5263d3d4d606c241c19ba78767c861a4caae69d
Mantaray Lab0 <mantaraylab0@gmail.com>
2023-08-02 11:06:15 -0700

    added a DVL publisher

---
+++ scripts/dvl_publisher.py
@@ -0,0 +1,33 @@
+from wldvl import WlDVL
+import rospy
+from mantaray_rpi.msg import DVL
+
+dvl = WlDVL("/dev/ttyUSB0", 115200)
+
+
+def dvl_publisher_callback():
+    rospy.init_node('dvl_publisher', anonymous=False)
+    rospy.loginfo("dvl_publisher node started")
+    pub = rospy.Publisher('dvl', String, queue_size=10)
+    
+    while not rospy.is_shutdown():
+        data = dvl.read()
+        
+        msg = DVL()
+        msg.header.stamp = rospy.Time.now()
+        msg.header.frame_id = 'world'
+        msg.velocity.x = data['vx']
+        msg.velocity.y = data['vy']
+        msg.velocity.z = data['vz']
+        msg.fom = data['fom']
+        msg.altitude = data['altitude']
+        msg.valid = data['valid']
+        
+        pub.publish(msg)
+        
+        rate = rospy.Rate(10) # 10hz
+        rate.sleep()
+
+
+if "__name__" == "__main__":
+    dvl_publisher_callback()
