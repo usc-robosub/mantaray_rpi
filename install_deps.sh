8189ec3cdba85ff73230886d936316f276499ac3
Matt Jiang <jiangmy@usc.edu>
2023-10-21 23:21:51 -0700

    fixed dependency scripts

--- install_deps.sh
+++ install_deps.sh
@@ -5,6 +5,8 @@ sudo apt-get install ros-melodic-pid
 sudo apt-get install libeigen3-dev
 sudo apt-get install libboost-all-dev
 
-cd ..
+cd scripts && pip3 install -r requirements.txt
+
+cd ../..
 git clone https://github.com/ANYbotics/kindr.git
 git clone https://github.com/ethz-adrl/control-toolbox.git
