ad29afd998ad854c0121ff9bed865e4a3e33f3e9
Lab Desktop <mantaraylab1@gmail.com>
2024-03-14 23:35:12 -0700

    create cascade pid for orientation

--- src/fsm/fsm_pcontrol.cpp
+++ src/fsm/fsm_pcontrol.cpp
@@ -1,5 +1,6 @@
 #include "include.h"
 #include "fsm.h"
+#include "std_msgs/Float64.h"
 
 
 extern double robotState[15];
@@ -32,20 +33,17 @@ void FSM_PControl::run(int dt){
     Eigen::Matrix<double, 3, 1> sp = this->ac.getAngularSetpoint(qsp, meas);
     
     
+    // this->thruster_values[0] = sp(2,0);
+    // this->thruster_values[1] = sp(2,0);
+    // this->thruster_values[2] = sp(2,0);
+    // this->thruster_values[3] = sp(2,0);
+    // this->thruster_values[4] = sp(0,0) - sp(1,0);
+    // this->thruster_values[5] = sp(0,0) - sp(1,0);
+    // this->thruster_values[6] = sp(0,0) + sp(1,0);
+    // this->thruster_values[7] = sp(0,0) + sp(1,0);
 
-    this->thruster_values[0] = sp(2,0);
-    this->thruster_values[1] = sp(2,0);
-    this->thruster_values[2] = sp(2,0);
-    this->thruster_values[3] = sp(2,0);
-    this->thruster_values[4] = sp(0,0) - sp(1,0);
-    this->thruster_values[5] = sp(0,0) - sp(1,0);
-    this->thruster_values[6] = sp(0,0) + sp(1,0);
-    this->thruster_values[7] = sp(0,0) + sp(1,0);
+
     
     ROS_INFO("Robot State: %f %f %f", robotState[3], robotState[4], robotState[5]);
     
  }
-
-
-
-
